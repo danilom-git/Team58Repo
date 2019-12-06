ea96e2b671dc6137c6b19db0ca19854a2b8c8199
djordje <Zoomb1xon>
2019-12-06 04:04:45 +0100

    post method one click controler

--- src/main/java/team58/healthy/controller/OneClickControler.java
+++ src/main/java/team58/healthy/controller/OneClickControler.java
@@ -1,13 +1,29 @@
 package team58.healthy.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team58.healthy.dto.OneClickDTO;
+import team58.healthy.model.OneClick;
+import team58.healthy.service.OneClickService;
+
+import javax.validation.constraints.PastOrPresent;
 
 
 @RestController
-@RequestMapping(value = "api/checkupTypes")
+@RequestMapping(value = "api/oneClickCheckup")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class OneClickControler {
+
+    @Autowired
+    private OneClickService oneClickService;
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<OneClickDTO> postOneClick(@RequestBody OneClickDTO oneClickDTO)
+    {
+        OneClickDTO ret = oneClickService.save(oneClickDTO);
+        return new ResponseEntity<>(ret, HttpStatus.OK);
+    }
 
 }
