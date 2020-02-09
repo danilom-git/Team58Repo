0ed471f313a448c20f18c36a2b8ffd835cbf119c
djordje <Zoomb1xon>
2020-02-09 19:23:32 +0100

    checkups for test added

--- src/main/java/team58/healthy/HealthyApplication.java
+++ src/main/java/team58/healthy/HealthyApplication.java
@@ -1,13 +1,19 @@
 package team58.healthy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import team58.healthy.service.DoctorService;
 
 @SpringBootApplication
 public class HealthyApplication {
 
+	@Autowired
+	private DoctorService doctorService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HealthyApplication.class, args);
 	}
+
 
 }
